0b87ddb85f64bcb7ea25096ca78ec60a337e0e76
Simon Templer <st@wetransform.to>
2026-07-07 08:43:45 +0000

    test: verify brotli and gzip compression

---
+++ src/test/kotlin/to/wetransform/nginxmetrics/CompressionTest.kt
@@ -0,0 +1,27 @@
+package to.wetransform.nginxmetrics
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import to.wetransform.nginxmetrics.support.NginxMetricsContainer
+
+class CompressionTest {
+
+  @Test
+  fun `responses are compressed according to Accept-Encoding`() {
+    NginxMetricsContainer().use { container ->
+      container.start()
+
+      val brotli = container.get("/", headers = mapOf("Accept-Encoding" to "br"))
+      assertEquals(200, brotli.statusCode())
+      assertEquals("br", brotli.headers().firstValue("Content-Encoding").orElse("<none>"))
+
+      val gzip = container.get("/", headers = mapOf("Accept-Encoding" to "gzip"))
+      assertEquals(200, gzip.statusCode())
+      assertEquals("gzip", gzip.headers().firstValue("Content-Encoding").orElse("<none>"))
+
+      val plain = container.get("/")
+      assertEquals(200, plain.statusCode())
+      assertEquals("<none>", plain.headers().firstValue("Content-Encoding").orElse("<none>"))
+    }
+  }
+}
